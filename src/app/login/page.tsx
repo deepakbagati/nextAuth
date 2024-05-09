abdba04ce35c4a55fd35c8389cbb9ef45e8ce7d5
deepakbagati <deepakbagati2708@gmail.com>
2024-05-09 22:46:59 +0530

    changes done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import {useRouter} from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
@@ -8,54 +8,46 @@ import { toast } from "react-hot-toast";
 
 
 
-export default function SignupPage() {
+
+export default function LoginPage() {
     const router = useRouter();
     const [user, setUser] = React.useState({
         email: "",
         password: "",
-        username: "",
+       
     })
     const [buttonDisabled, setButtonDisabled] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
 
-    const onSignup = async () => {
+
+    const onLogin = async () => {
         try {
             setLoading(true);
-            const response = await axios.post("/api/users/signup", user);
-            console.log("Signup success", response.data);
-            router.push("/login");
-            
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data);
+            toast.success("Login success");
+            router.push("/profile");
         } catch (error:any) {
-            console.log("Signup failed", error.message);
-            
+            console.log("Login failed", error.message);
             toast.error(error.message);
-        }finally {
-            setLoading(false);
+        } finally{
+        setLoading(false);
         }
     }
 
     useEffect(() => {
-        if(user.email.length > 0 && user.password.length > 0 && user.username.length > 0) {
+        if(user.email.length > 0 && user.password.length > 0) {
             setButtonDisabled(false);
-        } else {
+        } else{
             setButtonDisabled(true);
         }
     }, [user]);
 
-
     return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1>{loading ? "Processing" : "Signup"}</h1>
+        <h1>{loading ? "Processing" : "Login"}</h1>
         <hr />
-        <label htmlFor="username">username</label>
-        <input 
-        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="username"
-            type="text"
-            value={user.username}
-            onChange={(e) => setUser({...user, username: e.target.value})}
-            placeholder="username"
-            />
+        
         <label htmlFor="email">email</label>
         <input 
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
@@ -75,9 +67,9 @@ export default function SignupPage() {
             placeholder="password"
             />
             <button
-            onClick={onSignup}
-            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">{buttonDisabled ? "No signup" : "Signup"}</button>
-            <Link href="/login">Visit login page</Link>
+            onClick={onLogin}
+            className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">Login here</button>
+            <Link href="/signup">Visit Signup page</Link>
         </div>
     )
 
--- src/helpers/getDataFromToken.ts
+++ src/helpers/getDataFromToken.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from "next/server";
 import jwt from 'jsonwebtoken'
 
-export const getDataFromToken=(request:NextRequest){
+export const getDataFromToken=(request:NextRequest)=>{
     try{
         const token =request.cookies.get("token")?.value || '';
         const decodedToken=jwt.verify(token,process.env.TOKEN_SECRET!)   //token extract
